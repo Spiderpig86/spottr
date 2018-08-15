f2bd6b659f6bb1abf49c95866568ee7262b2f144
Spiderpig86 <slim679975@gmail.com>
2018-08-14 22:55:37 -0400

    feat(logging.service.ts): Added util logging service

---
+++ src/app/services/loggin-service/loggin.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '../../../../node_modules/@angular/core';
+
+export enum LOG_LEVEL {
+  None,
+  Severe,
+  Warning,
+  All
+}
+
+/**
+ * Service class for logging based on severity level
+ *
+ * @export
+ * @class LoggingService
+ */
+@Injectable()
+export class LoggingService {
+
+  private loggingLevel = LOG_LEVEL.All; // By default for development
+
+  constructor() {
+
+  }
+
+  /**
+   * Utility method for logging to console depending on severity level.
+   *
+   * @param message - message to log
+   * @param level - severity level of the log
+   */
+  public log(message: string, level: LOG_LEVEL) {
+    if (level < this.loggingLevel) {
+      console.log(message);
+    }
+  }
+
+  // HELPER FUNCTIONS
+  public setLogging(logging: LOG_LEVEL) {
+    this.loggingLevel = logging;
+  }
+
+  public getLogging(): LOG_LEVEL {
+    return this.loggingLevel;
+  }
+
+}
---
+++ src/app/services/spottr-service/spottr.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '../../../../node_modules/@angular/core';
+
+/**
+ * An Angular 6 service that interfaces with the Spotify API to fetch data.
+ */
+@Injectable()
+export class SpottrService {
+
+  private API_URL = 'https://api.spotify.com';
+
+  constructor() {
+
+  }
+}
