1ba3f5376f65ddefe922ae496135f5a6c2cdf747
Stanley Lim <slim679975@gmail.com>
2021-02-01 02:54:11 -0500

    :sparkles: feat(timestamp.pipe.ts): Added timestamp pipe

---
+++ src/app/modules/music/shared/pipes/timestamp.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'timestamp',
+})
+export class TimestampPipe implements PipeTransform {
+  transform(value: number) {
+    let seconds = Math.floor(value / 1000);
+    let minute = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+    let hour = Math.floor(minute / 60);
+    minute = minute % 60;
+
+    let time = `${minute.toString()}:${seconds.toString().padStart(2, '0')}`;
+    if (hour) {
+      time = `${hour}:${time}`;
+    }
+    return time;
+  }
+}
