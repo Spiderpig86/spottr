872c44094845e1d853523a0a3b50feb9cedea970
Stanley Lim <slim679975@gmail.com>
2021-05-31 18:46:52 -0400

    :sparkles: feat(store.ts): Integrated LocalStorageService with store to store temporary login tokens

--- src/app/modules/auth/shared/shared.module.ts
+++ src/app/modules/auth/shared/shared.module.ts
@@ -3,17 +3,18 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 
 import { AuthService } from './services/auth.service';
 import { AuthGuard } from './guards/auth.guard';
+import { AppSharedModule } from '../../shared/shared.module';
 
 @NgModule({
-    imports: [CommonModule],
-    declarations: [],
-    providers: []
+  imports: [CommonModule, AppSharedModule],
+  declarations: [],
+  providers: [],
 })
 export class SharedModule {
-    static forRoot(): ModuleWithProviders<SharedModule> {
-        return {
-            ngModule: SharedModule,
-            providers: [AuthService, AuthGuard]
-        }
-    }
+  static forRoot(): ModuleWithProviders<SharedModule> {
+    return {
+      ngModule: SharedModule,
+      providers: [AuthService, AuthGuard],
+    };
+  }
 }
--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,7 @@
 import { BehaviorSubject, Observable } from 'rxjs';
 import { distinctUntilChanged, pluck } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+import { LocalStorageService } from './app/modules/shared/local-storage.service';
 
 export interface State {
   access_token: string | undefined;
@@ -15,6 +16,13 @@ export class Store {
   private subject = new BehaviorSubject<State>(state);
   private store = this.subject.asObservable().pipe(distinctUntilChanged());
 
+  constructor(private localStorageService: LocalStorageService) {
+    // Initialize state from local storage
+    Object.keys(state).forEach(key => {
+      state[key] = localStorageService.getItem(key);
+    });
+  }
+
   get value() {
     return this.subject.value;
   }
@@ -23,7 +31,8 @@ export class Store {
     return this.store.pipe(pluck(name));
   }
 
-  set(name: string, state: any) {
-    this.subject.next({ ...this.value, [name]: state });
+  set(name: string, value: any) {
+    this.subject.next({ ...this.value, [name]: value });
+    this.localStorageService.setItem(name, value);
   }
 }
